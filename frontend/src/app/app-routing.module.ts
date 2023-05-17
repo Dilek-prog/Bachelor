ea8e8c5e94bb2a4e8ef8bd093017f0152d75cf61
Dilek-prog <82120659+Dilek-prog@users.noreply.github.com>
2023-05-17 19:53:17 +0200

    add Ansicht to navbar

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnsichtComponent } from './ansicht/ansicht.component';
 import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
   {
-    path:"",
+    path:"home",
     component: HomeComponent
+  },
+  {
+    path:"ansicht",
+    component: AnsichtComponent
   }
 ];
 
