071600954ec6cca1b72af3767930eabd43ec5ae5
Dilek-prog <82120659+Dilek-prog@users.noreply.github.com>
2023-05-23 19:04:04 +0200

    add Backend with API

--- backend/slackbridge/autopost/models.py
+++ backend/slackbridge/autopost/models.py
@@ -1,3 +1,15 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.TextField()
+    text = models.TextField()
+    pub_date = models.DateTimeField() #publication date
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    channel = models.TextField()
+    posted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user} -> {self.title} ({self.pub_date})"
+
--- backend/slackbridge/autopost/views.py
+++ backend/slackbridge/autopost/views.py
@@ -1,3 +1,15 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from .models import Post
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import PostSerializer
 
 # Create your views here.
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permissions_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        queryset = Post.objects.filter(title="teszt")
