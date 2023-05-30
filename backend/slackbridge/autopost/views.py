4dc79c59d4098f3574fa3fd3ad9ec8589cf551c2
Dilek-prog <82120659+Dilek-prog@users.noreply.github.com>
2023-05-30 21:31:33 +0200

    cut APIView

--- backend/slackbridge/autopost/views.py
+++ backend/slackbridge/autopost/views.py
@@ -1,3 +1,4 @@
+from .permission import IsOwner
 from django.db.models.query import QuerySet
 from django.http import HttpResponse, JsonResponse
 from django.http.response import Http404
@@ -8,46 +9,24 @@ from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework import status
+from rest_framework import generics
 
 from .models import Post
 from .serializers import PostSerializer
 
-class PostList(APIView):
-    def get(self, request, format=None):
-        posts = Post.objects.all()  
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
+class PostList(generics.ListCreateAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-    def post(self, request, format=None):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(user=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+    
+    def get_queryset(self):
+        return Post.objects.filter(user=self.request.user)
 
-class PostDetail(APIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
 
-    def get_object(self, pk):
-        try:
-            return Post.objects.get(pk=pk)
-        except Post.DoesNotExist:
-            raise Http404
 
-    def get(self, request, pk):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        post = self.get_object(pk)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
