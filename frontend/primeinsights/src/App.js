e189e78020c29a4d9493eb2bfa4636a4c815f769
Dilek-prog <82120659+Dilek-prog@users.noreply.github.com>
2023-06-26 10:50:05 +0200

    Css Home

--- frontend/primeinsights/src/App.js
+++ frontend/primeinsights/src/App.js
@@ -4,6 +4,9 @@ import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Table from 'react-bootstrap/Table';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar'; 
+import Image from 'react-bootstrap/Image';
 import {useState, useRef} from 'react';
 import axios from 'axios';
 import {useMutation, useQuery} from '@tanstack/react-query';
@@ -15,6 +18,8 @@ import {faCircleCheck, faClock} from '@fortawesome/free-solid-svg-icons';
 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarToggle from 'react-bootstrap/esm/NavbarToggle';
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
 
 dayjs.extend(relativeTime);
 dayjs.locale('de');
@@ -25,7 +30,6 @@ const API_POSTS = "/posts/";
 
 
 function LoginWindow({onReceivedToken}) {
-
   let userRef = useRef(); // speichern des veränderlichen Werts ohne bei aktualisiserung ein erneutes Rendern zu verursachen
   let passwordRef = useRef();
 
@@ -54,11 +58,19 @@ function LoginWindow({onReceivedToken}) {
     },
   });
 
-function handleClick(){ //sobald Login geklickt wird, wird das Ereignis handleClick getätigt aber nur, wenn die Daten vom Lgin stimmen 
+function handleClick(){ //sobald Login geklickt wird, wird das Ereignis handleClick getätigt aber nur, wenn die Daten vom Login stimmen 
     loginMutation.mutate();
   }
 
   return ( //Template Bootstrap 
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-revers">
+        <div>
+          <h1 class="italic">Herzlich Willkomen zu PrimeInsights</h1>
+          <h3 className="md:not-italic">Haben Sie es satt nach ihren Retourenscheinen oder Passwörter zu suchen? legen sie darauf, wert alles auf einem Platz zu haben und das alles transparent und gesichert?
+          Dann sind sie bei uns genau richtig. Registrieren Sie sich kostenlos bei uns und erwarten Sie eine qualitative und effiziente Einsicht Ihrer Retoure und Zahlungsinformation.</h3>
+        </div>
+        <div className="actions flex gap-4">
     <Container fluid>  
       <Card style={{ width: '18rem' }}>
         <Card.Body>
@@ -82,7 +94,11 @@ function handleClick(){ //sobald Login geklickt wird, wird das Ereignis handleCl
         </Card.Body>
       </Card>
     </Container>
+        </div>
+      </div>
+    </div>
   );
+  
 }
 
 function PostList({token}) {
@@ -124,7 +140,7 @@ function PostList({token}) {
   )
 
   return (
-    <Container fluid>
+    <Container class="table" fluid>
       <Table striped bordered hover>
         <thead>
           <tr>
@@ -145,7 +161,7 @@ function PostList({token}) {
                 <td>{post.pub_date.fromNow()}</td>
                 <td>{post.created.format('DD.MM.YYYY')}</td> 
                 <td>{post.channel}</td>
-                <td>{post.posted?<FontAwesomeIcon icon={faCircleCheck}/>:<FontAwesomeIcon icon={faClock}/>}</td>
+                <td className="posted-icon" >{post.posted?<FontAwesomeIcon icon={faCircleCheck}/>:<FontAwesomeIcon icon={faClock}/>}</td>
                 <td>{post.text}</td>
               </tr>
             )
@@ -157,10 +173,61 @@ function PostList({token}) {
      
   );
 }
+function Homepage({token}) {
+  return (
+    <>
+      <div>
+        <Navbar sticky="top" bg="light" expand="lg" className="navbar">
+        <Image src="favicon.ico" className="img" alt="" rounded />
+            <NavbarToggle aria-controls="basic-navbar-nav"/>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#Ansicht">Ansicht</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+        
+    <section className="hero hero-bg d-flex justify-content-center align-items-center">    
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6 col-md-10 col-12 d-flex flex-column justify-content-center align-items-center">
+           <div className="hero-text">
+              <h1 className="text-white aos-init aos-animate" data-aos="fade-up">
+               <strong className="h1">Problem:</strong> "Interviewpartner bei Bewerbungsgesprächen werden derzeit rein manuell ausgesucht, was einen hohen Zeit und Arbeitsaufwand mit sich bringt."
+                 <p></p>
+                  <p class="p">
+                    <strong className="Interview">
+                      </strong>"Interview Matching - simplify the hiring process."
+                  </p>
+              </h1>
+                <div className="col-lg-6 col-12">
+                  <div className="hero-image aos-init aos-animate" data-aos="fade-up" data-aos-delay="300">
+                    <img src="-..." className="img-fluid" alt="img">
+                    </img>                 
+                  </div>
+                </div>
+
+            <div className="d-flex justify-content-center justify-content-lg-start">
+            <Button className="about" class="btn-get-started scrollto">Create Post</Button>
+            </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    </section>
+      </div>
+      <PostList token={token}/>
+    </>
+  )
+}
+
+
+
 
 function App() {
   let [token, setToken] = useState(''); // Status einer Funktionskomponente verfolgen
-
+  
   function handleReceivedToken(newToken) {
     console.log(newToken);
     setToken(newToken);
@@ -169,12 +236,14 @@ function App() {
   if (token === '') {
     return (
       <LoginWindow onReceivedToken={handleReceivedToken}/> 
-    );
-  } else {
-    return (
-      <PostList token={token}/>
-    )  
-  }
-}
+      );
+    } else {
+      return (
+        <Homepage token={token}/>
+        )  
+      }
+    }
+    
 
-export default App;
+    export default App;
+    
