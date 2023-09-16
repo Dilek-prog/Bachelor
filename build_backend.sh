c7c7e2faf8618199123f7c0257228c39199d14db
Dilek-prog <82120659+Dilek-prog@users.noreply.github.com>
2023-09-16 16:55:16 +0200

    add volumes to build_backend

--- build_backend.sh
+++ build_backend.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker run -it bachelor-backend:latest python3 /app/manage.py makemigrations
-docker run -it bachelor-backend:latest python3 /app/manage.py migrate
-docker run -it bachelor-backend:latest python3 /app/manage.py createsuperuser
+docker run -it -v ${PWD}/backend/slackbridge/db.sqlite3:/app/db.sqlite3:z bachelor-backend:latest python3 /app/manage.py makemigrations
+docker run -it -v ${PWD}/backend/slackbridge/db.sqlite3:/app/db.sqlite3:z bachelor-backend:latest python3 /app/manage.py migrate
+docker run -it -v ${PWD}/backend/slackbridge/db.sqlite3:/app/db.sqlite3:z bachelor-backend:latest python3 /app/manage.py createsuperuser
 
