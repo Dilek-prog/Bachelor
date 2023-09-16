589c8d177642284bb5c7561c2796ca36f28ff818
Dilek-prog <82120659+Dilek-prog@users.noreply.github.com>
2023-09-16 17:42:09 +0200

    delete an use build script

--- build_backend.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-docker run -it -v ${PWD}/backend/slackbridge:/app:z bachelor-backend:latest python3 /app/manage.py makemigrations
-docker run -it -v ${PWD}/backend/slackbridge:/app:z bachelor-backend:latest python3 /app/manage.py migrate
-docker run -it -v ${PWD}/backend/slackbridge:/app:z bachelor-backend:latest python3 /app/manage.py createsuperuser
-
