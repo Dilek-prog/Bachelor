4b2ea5ab868a5bfaae4871e7b46c27908efeb03b
Dilek-prog <82120659+Dilek-prog@users.noreply.github.com>
2023-09-16 16:29:24 +0200

    add domain to allowed host

--- backend/slackbridge/slackbridge/settings.py
+++ backend/slackbridge/slackbridge/settings.py
@@ -28,7 +28,7 @@ SLACK_API_TOKEN = os.environ.get("SLACK_API_TOKEN", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["primeinsights.space"]
 
 
 # Application definition
