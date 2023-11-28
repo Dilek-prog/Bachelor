525b6f24970c1e5f4f3b9908017808e4c3c16b2b
Dilek-prog <82120659+Dilek-prog@users.noreply.github.com>
2023-11-28 14:39:44 +0100

    add comments

--- backend/slackbridge/autopost/cron.py
+++ backend/slackbridge/autopost/cron.py
@@ -5,12 +5,13 @@ from django_cron import CronJobBase, Schedule
 from .models import Post
 
 class PublishPosts(CronJobBase):
-    RUN_EVERY_MINS = 1
+    RUN_EVERY_MINS = 1 #Die Aufgabe wird alle 1 Minute ausgeführt 
 
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
-    code = 'autopost.PublishPost' 
+    code = 'autopost.PublishPost'  #Eindeutiger Code für die Aufgabe
 
     def do(self):
+        #Holt ungepostete Blog-Posts, die veröffentlicht werden können
         posts = Post.objects.filter(posted=False, pub_date__lte=datetime.now().astimezone(), error="")
         print(f"Publishing {len(posts)} posts ({posts})")
         for post in posts:
